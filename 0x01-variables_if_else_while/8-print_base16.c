001c058dd0636a83bb6b91da40d86db8f74492d6
iranziprince <iranziprince2001@gmail.com.com>
2022-10-14 18:08:33 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,8 @@ int main(void)
 	int n;
 	char l;
 
-	for (n = 0; n < 16; n++)
-		putchar((n % 16) + '0');
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
 
 	for (l = 'a'; l <= 'f'; l++)
 		putchar(l);
