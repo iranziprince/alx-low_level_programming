ba0f5c20c803c5af42bee67077f3010944d66f6e
iranziprince <iranziprince2001@gmail.com.com>
2022-10-17 10:03:34 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,12 +10,12 @@ int main(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		for (j = 1; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			putchar((i % 9) + '0');
+			putchar((i % 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i == 9 && j == 9)
+			if (i == 8 && j == 9)
 				continue;
 
 			putchar(',');
