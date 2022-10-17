26bd9af7a3439f05e6ded8a328fd3711fa79657e
iranziprince <iranziprince2001@gmail.com.com>
2022-10-16 17:17:09 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - Prints the numbers from 00 to 99, numbers separated
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 9 && j == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
