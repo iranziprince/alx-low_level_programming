78a27b6d440171efca1202164ed5f458ecf68e3a
iranziprince <iranziprince2001@gmail.com.com>
2022-10-17 12:50:55 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+ * main - print combination of 3 numbers
+ * Return: Always 0
+ */
+int main (void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
+
+				if (i == 7 && j == 8 && k== 9)
+					continue;
+			}
+		}
+	}
+	putchar(',');
+	putchar(' ');
+	return (0);
+}
