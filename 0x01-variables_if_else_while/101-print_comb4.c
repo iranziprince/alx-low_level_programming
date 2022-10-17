a90d75130884aaf0006f7c571d5446ea4d1e3085
iranziprince <iranziprince2001@gmail.com.com>
2022-10-17 13:41:52 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,7 +19,7 @@ int main(void)
 				putchar((j % 10) + '0');
 				putchar((k % 10) + '0');
 
-				if (i == 8 && j == 9 && k == 10)
+				if (i == 7 && j == 8 && k == 9)
 					continue;
 
 				putchar(',');
