42de3887913c0d190182d7c3b59feb9de0066178
iranziprince <iranziprince2001@gmail.com.com>
2022-10-18 15:59:18 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * is written by prince
+ * main - prints _putchar and a new line
  * Return: Always 0
  */
 int main(void)
@@ -16,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
