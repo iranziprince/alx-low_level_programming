dd511078151f38012f0f4a44df492efeddb42e7a
iranziprince <iranziprince2001@gmail.com.com>
2022-10-18 17:28:42 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,16 @@
  */
 int print_last_digit(int n)
 {
-	int ld;
-	ld = n % 10;
+	int last;
 
-	if (ld < 0)
+	last = n % 10;
+
+	if (last < 0)
 	{
-		ld = ld * -1;
+		last = last * -1;
 	}
 
-	_putchar(ld + '0');
+	_putchar(last + '0');
 
-	return (0);
+	return (last);
 }
