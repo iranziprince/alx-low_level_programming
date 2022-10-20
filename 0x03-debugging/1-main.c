6764a6f77a3970596b898d2e88eb8952196bd61c
iranziprince <iranziprince2001@gmail.com.com>
2022-10-20 13:29:20 -0700

    Task 0

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,14 +13,11 @@ int main(void)
 	i = 0;
 
 	/*while (i < 10)*/
-	while (i < 10)
 	/*{*/
-	{
-		/*	putchar(i);*/
-		putchar(i);
-		/*}*/
-}
-printf("Infinite loop avoided! \\o/\n");
+	/*	putchar(i);*/
+	/*}*/
 
-return (0);
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0)
 }
