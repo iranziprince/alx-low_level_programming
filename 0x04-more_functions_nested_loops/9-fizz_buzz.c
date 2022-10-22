6fab42f8126ac1aa8270f6b5936045f9c4b58bd7
iranziprince <iranziprince2001@gmail.com.com>
2022-10-22 12:18:35 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,17 +8,26 @@ int main(void)
 {
 	int n;
 
-	print("1");
-	for (n = 2; n <= 100; n++)
+	for (n = 1; n <= 100; n++)
 	{
-		printf(" ");
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
 			printf("Fizz");
-		if (n % 5 == 0)
-		printf("Buzz");
-		if (n % 3 != 0 && n % 5 != 0)
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
 			printf("%d", n);
+		}
+
+		printf("\n");
+		return (0);
 	}
-	printf("\n");
-	return (0);
 }
