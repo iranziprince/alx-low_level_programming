6b26b9e02550d8b9f3c7c10e367503d0ba6d29e7
iranziprince <iranziprince2001@gmail.com.com>
2022-10-31 15:15:38 -0700

    Task 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 void *_memset(void *s, int c, size_t n);
-char *_memcpy(char *dest, char *src, unsigned int n);
+void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
