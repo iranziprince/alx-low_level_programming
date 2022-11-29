6c0a70ab2fc2281b3aac0925025cd2e4208018b1
iranziprince <iranziprince2001@gmail.com>
2022-11-29 09:15:37 -0800

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,11 +5,13 @@
  * print_list - Prints all the elements of a list_t list
  * @h: The list_t list
  *
- * Return: The number of nodes in h
- */
+ * Return: The number of nodes h
+ */c
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
+	unsigned int len;
+	char str, next;
 
 	while (h)
 	{
