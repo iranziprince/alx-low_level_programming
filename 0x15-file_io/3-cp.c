bcc2bbf5da696a5987646ceac36160f91ef3ae4c
iranziprince <iranziprince2001@gmail.com>
2022-12-08 08:49:18 -0800

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", arg
+					"Error: Can't read from file %s\n", argv
 					[1]);
 			free(buffer);
 			exit(98);
